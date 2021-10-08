7816eec466c21f9f5fd1fc486cc77d4558e4f7d0
bilalallylab <bilal@allylab.co>
2021-10-08 18:37:38 +0500

    scrapy using itemloader

--- practice_one/spiders/quotes_spider.py
+++ practice_one/spiders/quotes_spider.py
@@ -1,4 +1,22 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose, Join
+from w3lib.html import remove_tags
+
+
+class QuotesItem(scrapy.Item):
+    quote_text = scrapy.Field(
+        input_processor=MapCompose(remove_tags),
+        output_processor=TakeFirst()
+    )
+    author = scrapy.Field(
+        input_processor=MapCompose(remove_tags),
+        output_processor=TakeFirst()
+    )
+    tags = scrapy.Field(
+        input_processor=MapCompose(remove_tags),
+        output_processor=TakeFirst()
+    )
 
 
 class QuotesSpider(scrapy.Spider):
@@ -8,14 +26,15 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('span small::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-            }
+        for quote in response.xpath('//div[@class="quote"]'):
+            l = ItemLoader(item=QuotesItem(), selector=quote)
+            l.add_xpath('quote_text', './/span/text()')
+            l.add_xpath('author', './/span[2]/small/text()')
+            l.add_xpath('tags', './/div/a/text()')
 
-        next_page = response.css('li.next a::attr(href)').get()
+            yield l.load_item()
+
+        next_page = response.css('.next a').attrib['href']
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
