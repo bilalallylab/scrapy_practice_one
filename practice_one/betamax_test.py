f61962029575f2404e6e6523a9e585b5fe27a476
bilalallylab <bilal@allylab.co>
2021-11-02 22:24:54 +0500

    adding betamax for testing

---
+++ practice_one/betamax_test.py
@@ -0,0 +1,38 @@
+import betamax
+from spiders.quotes_spider import QuotesSpider
+import os
+import json
+from scrapy.http import HtmlResponse
+from betamax.fixtures.unittest import BetamaxTestCase
+
+
+CASSETTE_LIBRARY_DIR = 'cassettes_result'
+if not os.path.exists(CASSETTE_LIBRARY_DIR):
+    os.mkdir(CASSETTE_LIBRARY_DIR)
+
+with betamax.Betamax.configure() as config:
+    config.cassette_library_dir = CASSETTE_LIBRARY_DIR
+    config.preserve_exact_body_bytes = True
+
+
+class Testing(BetamaxTestCase):
+    def load_json(self, file_path):
+        with open(file_path) as json_file:
+            data = json.load(json_file)
+        return data
+
+    def test_parse(self):
+        sp_obj = QuotesSpider()
+
+        response = self.session.get(sp_obj.start_urls[0])
+
+        scrapy_response = HtmlResponse(body=response.content, url=sp_obj.start_urls[0])
+
+        result = sp_obj.parse(scrapy_response)
+
+        for each_val in self.load_json(file_path="quotes.json"):
+            self.assertEqual(each_val, result.next())
+
+        with self.assertRaises(StopIteration):
+            result.next()
+
